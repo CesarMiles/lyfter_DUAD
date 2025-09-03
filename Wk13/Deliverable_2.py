b81f224e752b3823509ef697bf8428f5d18dd648
Cesar Miles <ca.miles@hotmail.com>
2025-09-03 12:22:25 -0600

    Adding corrections based on feedback received. Deliverable_1 showing parameter **kwargs and Deliverable_2 validation for **kwargss

--- Wk13/Deliverable_2.py
+++ Wk13/Deliverable_2.py
@@ -1,18 +1,27 @@
 def int_only(func):
-    def wrapper(*args):
+    def wrapper(*args, **kwargs):
         for arg in args:
             if not isinstance(arg,(int, float)):
                 raise TypeError (f'The parameters {arg} must be a number')
-        result = func(*args)
+        
+        for key, value in kwargs.items():
+            if not isinstance(value, (int, float)):
+                raise TypeError(f'The parameter {key}={value} must be a number')
+            
+        result = func(*args, **kwargs)
         return result
 
 
     return wrapper
 
 @int_only
-def sum_two_numbers(a, b):
-    return a + b
+def calculate_total_price(price, quantity, discount=0):
+    return price * quantity * (1 - discount)
 
 
-sumarization = sum_two_numbers(3, 2)
-print(sumarization)
+print(calculate_total_price(100, 2, discount=0.1))
+
+try:
+    print(calculate_total_price(100, 2, discount='10%'))
+except TypeError as e:
+    print(f'Error found: {e}')
