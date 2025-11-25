c48e45e9eb3f998364a7471230ef4c651100a575
Cesar Miles <ca.miles@hotmail.com>
2025-11-24 20:21:05 -0600

    Adding additional functions to each class, now all methods can be called and they will be executed at once. Now pending to add feature to assign car to User

--- Modulo_2/BEWK06/Deliverable_Wk06.py
+++ Modulo_2/BEWK06/Deliverable_Wk06.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey, SmallInteger, insert, update, delete
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey, SmallInteger, insert, update, delete, select
 
 DB_URI = 'postgresql://postgres:pass123@localhost:5432/postgres'
 engine = create_engine(DB_URI, echo=True)
@@ -47,50 +47,125 @@ except Exception as e:
     print('Connection failed:', e)
 
 class UserRepository:
+    def __init__(self, connection):
+        self.conn = connection
+    
     def create(self, **kwargs):
-        return insert(users_table).values(**kwargs).returning(users_table.c.user_id, users_table.c.full_name)
+        stmt = insert(users_table).values(**kwargs).returning(users_table.c.user_id, users_table.c.full_name)
+        result = self.conn.execute(stmt)
+        self.conn.commit()
+        return result.scalar()
     
     def modify(self, modify_user_id, **kwargs):
-        if kwargs:
-            return update(users_table).where(users_table.c.user_id == modify_user_id).values(**kwargs)
-        else:
-            return print(f'There are no values to modify.')
+        if not kwargs:
+            print(f'There are no values to modify.')
+            return None
+        
+        stmt = update(users_table).where(users_table.c.user_id == modify_user_id).values(**kwargs)
+        result = self.conn.execute(stmt)
+        self.conn.commit()
+        return result.rowcount
     
     def delete(self, user_id_to_delete):
-        return delete(users_table).where(users_table.c.user_id == user_id_to_delete)
-    
-    def get_users(self):
+        stmt = delete(users_table).where(users_table.c.user_id == user_id_to_delete)
+        self.conn.execute(stmt)
+        self.conn.commit()
+        print(f'User {user_id_to_delete} has been deleted')
         return 
     
+    def get_users(self):
+        stmt = select(users_table)
+        result = self.conn.execute(stmt)
+        return result.all()
+    
 class AddressRepository:
+    def __init__(self, connection):
+        self.conn = connection
+
     def create(self, **kwargs):
-        return insert(address_table).values(**kwargs).returning(address_table.c.address_id, address_table.c.user_id)
+        stmt = insert(address_table).values(**kwargs).returning(address_table.c.address_id, address_table.c.user_id)
+        result = self.conn.execute(stmt)
+        self.conn.commit()
+        return result.scalar()
     
     def modify(self, modify_address_id, **kwargs):
-        if kwargs:
-            return update(address_table).where(address_table.c.address_id == modify_address_id).values(**kwargs)
-        else:
-            return print('There are no values to modify')
+        if not kwargs:
+            print('There are no values to modify')
+            return None
+        stmt = update(address_table).where(address_table.c.address_id == modify_address_id).values(**kwargs)
+        result = self.conn.execute(stmt)
+        self.conn.commit()
+        return result.rowcount
     
     def delete(self, address_id_to_delete):
-        return delete(address_table).where(address_table.c.address_id == address_id_to_delete)
+        stmt = delete(address_table).where(address_table.c.address_id == address_id_to_delete)
+        self.conn.execute(stmt)
+        self.conn.commit()
+        print(f'Address {address_id_to_delete} has been deleted')
+        return
+    
+    def get_address(self):
+        stmt = select(address_table)
+        result = self.conn.execute(stmt)
+        return result.all()
 
 class CarRepository:
+    def __init__(self, connection):
+        self.conn = connection
+
+
     def create(self, **kwargs):
-        return insert(car_table).values(**kwargs).returning(car_table.c.car_id, car_table.c.brand)
+        stmt = insert(car_table).values(**kwargs).returning(car_table.c.car_id, car_table.c.brand)
+        result = self.conn.execute(stmt)
+        self.conn.commit()
+        return result.scalar()
     
     def modify(self, modify_car_id, **kwargs):
-        if kwargs:
-            return update(car_table).where(car_table.c.car_id == modify_car_id).values(**kwargs)
-        else:
-            return print(f'There are no values to modify')
-    
+        if not kwargs:
+            print('There are no values to modify')
+            return None
+        
+        stmt = update(car_table).where(car_table.c.car_id == modify_car_id).values(**kwargs)
+        result = self.conn.execute(stmt)
+        self.conn.commit()
+        return result.rowcount
+        
     def delete(self, car_id_to_delete):
-        return delete(car_table).where(car_table.c.car_id == car_id_to_delete)
+        stmt = delete(car_table).where(car_table.c.car_id == car_id_to_delete)
+        self.conn.execute(stmt)
+        self.conn.commit()
+        print(f'Car {car_id_to_delete} has been deleted')
+        return
 
-user = UserRepository()
-car = CarRepository()
+    def get_cars(self):
+        stmt = select(car_table)
+        result = self.conn.execute(stmt)
+        return result.all()
+
+
+# with engine.connect() as conn:
+#     address_repo = AddressRepository(conn)
+
+#     new_address = address_repo.create(country="Costa Rica", province="Alajuela", city="Grecia", user_id=9)
+#     print(f'New address ID: {new_address}')
+
+#     all_address = address_repo.get_address()
+#     for address in all_address:
+#         print(f"ID: {address.address_id}, User Id: {address.user_id}")
+
+#     rows_updated = address_repo.modify(new_address, city="Grecia City")
+#     print(f"Filas actualizadas: {rows_updated}")
+
 
 with engine.connect() as conn:
-    conn.execute(car.delete(1))
-    conn.commit()
+    car_repo = CarRepository(conn)
+
+    new_car = car_repo.create(model="Seagul", brand="BYD", factory_year=2020)
+    print(f'New Car ID: {new_car}')
+
+    all_cars = car_repo.get_cars()
+    for car in all_cars:
+        print(f"ID: {car.car_id}, User Id: {car.user_id}")
+
+    rows_updated = car_repo.modify(new_car, user_id=9)
+    print(f"Filas actualizadas: {rows_updated}")
