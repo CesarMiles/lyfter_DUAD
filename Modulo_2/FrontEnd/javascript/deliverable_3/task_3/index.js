83c621def039554d9123baf7cdb60516ea860608
Cesar Miles <ca.miles@hotmail.com>
2026-03-07 17:57:51 -0600

    Adding correction to only change color on <p>

--- Modulo_2/FrontEnd/javascript/deliverable_3/task_3/index.js
+++ Modulo_2/FrontEnd/javascript/deliverable_3/task_3/index.js
@@ -1,9 +1,9 @@
-const textContainer = document.querySelector('.text-container')
+const text = document.querySelector('.text-container p')
 const button = document.querySelector('.change-button')
 const colorArray = ["red", "blue", "green", "yellow", "cyan", "pink"]
 
 button.addEventListener('click', () => {
   const randomIndex = Math.floor(Math.random() * colorArray.length);
   const randomColor = colorArray[randomIndex];
-  textContainer.style.backgroundColor = randomColor;
+  text.style.backgroundColor = randomColor;
 })
