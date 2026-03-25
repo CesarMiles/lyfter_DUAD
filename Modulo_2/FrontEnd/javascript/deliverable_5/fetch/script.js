abbd82bea51b125c092c35318590a2cd017c8f59
Cesar Miles <ca.miles@hotmail.com>
2026-03-25 12:58:48 -0600

    Adding task 4 from fetch section from deliverable 5 js

--- Modulo_2/FrontEnd/javascript/deliverable_5/fetch/script.js
+++ Modulo_2/FrontEnd/javascript/deliverable_5/fetch/script.js
@@ -66,3 +66,35 @@ getDataById('ff8081819d150699019d2293768e17b0')
 // }
 
 // postUser('Cesar M', 'Cesar@test.com', 'pass123', 'CR, Grecia')
+
+async function updateUserAddress (id, newAddres) {
+  try {
+    const updateBody = {
+      'data' : {
+        'address': newAddres
+      }};
+      
+    const response = await fetch(`https://api.restful-api.dev/objects/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updateBody)
+    });
+
+    if (response.status === 404) {
+      throw new Error(`HTTP error. Status: ${fetchData.status}, User not found`);
+    };
+
+    if (!response.ok) {
+      throw new Error(`HTTP error. Status: ${response.status}`);
+    };
+
+    const data = await response.json();
+    console.log(data);
+  } catch {
+    console.error('Error creating post', error)
+  }
+}
+
+updateUserAddress('ff8081819d150699019d2293768e17b0', 'Costa Rica')
