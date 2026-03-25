7bad22d543224a00e1d71a51c7b37ec8ca40adde
Cesar Miles <ca.miles@hotmail.com>
2026-03-24 19:23:12 -0600

    Adding task 2 and 3 from the Fetch section of deliverable 5 js

--- Modulo_2/FrontEnd/javascript/deliverable_5/fetch/script.js
+++ Modulo_2/FrontEnd/javascript/deliverable_5/fetch/script.js
@@ -2,7 +2,7 @@ async function getData(){
   try {
     const fetchData = await fetch('https://api.restful-api.dev/objects');
     const data =  await fetchData.json();
-    // console.log(data);
+
     data.forEach((object) => {
       if (object.data) {
         const objectPropieties = Object.keys(object.data);
@@ -14,5 +14,55 @@ async function getData(){
     console.log('There was an error', {error});
   }}
 
-
 getData();
+
+async function getDataById(id) {
+  try {
+    const fetchData = await fetch(`https://api.restful-api.dev/objects/${id}`);
+    if (fetchData.status === 404) {
+      throw new Error(`HTTP error. Status: ${fetchData.status}, User not found`);
+    };
+
+    const data = await fetchData.json();
+
+    const dataPropieties = Object.keys(data.data)
+    console.log(`${data.name} (${dataPropieties.map((key) => {
+          return `${key}: ${data.data[key]}`}).join(', ')}`)
+
+  } catch (error) {
+    console.error('Error getting user', error)
+  }
+}
+
+getDataById('ff8081819d150699019d2293768e17b0')
+// Valid ID: ff8081819d150699019d2293768e17b0
+
+// async function postUser(name, email, password, address) {
+//   try {
+//     const userBody = {
+//         'name': name,
+//         'data': {
+//           'email': email,
+//           'password': password,
+//           'address': address
+//         }}
+//     const response = await fetch('https://api.restful-api.dev/objects', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(userBody)
+//     });
+//     if (!response.ok) {
+//       throw new Error(`HTTP error. Status: ${response.status}`);
+//     };
+
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error('Error creating post', error)
+//   }
+  
+// }
+
+// postUser('Cesar M', 'Cesar@test.com', 'pass123', 'CR, Grecia')
