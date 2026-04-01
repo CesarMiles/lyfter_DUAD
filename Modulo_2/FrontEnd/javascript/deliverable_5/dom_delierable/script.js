c7bf4ae04bad515798fc40b80b94dee7e33ac3b5
Cesar Miles <ca.miles@hotmail.com>
2026-03-31 20:12:19 -0600

    Adding script.js and login page. Pending to add axios functions and homepage for update password

---
+++ Modulo_2/FrontEnd/javascript/deliverable_5/dom_delierable/script.js
@@ -0,0 +1,74 @@
+const form = document.getElementById('form')
+const nameInput = document.getElementById('name-input')
+const emailInput = document.getElementById('email-input')
+const passwordInput = document.getElementById('password-input')
+const addressInput = document.getElementById('address-input')
+const userIdInput = document.getElementById('user-id-input')
+const errorMessage = document.getElementById('error-message')
+
+form.addEventListener('submit', (e) => {
+  
+
+  let errors = []
+
+  if(nameInput) {
+    
+    errors = getSignupFormErrors(nameInput.value, emailInput.value, passwordInput.value, addressInput.value)
+  }
+  else {
+    errors = getLoginFormErrors(userIdInput.value, passwordInput.value)
+  }
+
+  if (errors.length > 0){
+    e.preventDefault()
+    errorMessage.innerText = errors.join(". ")
+  }
+})
+
+function getSignupFormErrors(name, email, password, address) {
+  let errors = []
+
+  if (name === '' || name == null){
+    errors.push('Name is required')
+    nameInput.parentElement.classList.add('incorrect')
+  }
+  if (email === '' || email == null){
+    errors.push('Email is required')
+    emailInput.parentElement.classList.add('incorrect')
+  }
+  if (password === '' || password == null){
+    errors.push('Password is required')
+    passwordInput.parentElement.classList.add('incorrect')
+  }
+  if (address === '' || address == null){
+    errors.push('Address is required')
+    addressInput.parentElement.classList.add('incorrect')
+  }
+  return errors;
+}
+
+function getLoginFormErrors(userId, password) {
+  let errors = []
+
+  if (userId === '' || userId == null){
+    errors.push('User Id is required')
+    userIdInput.parentElement.classList.add('incorrect')
+  }
+  if (password === '' || password == null){
+    errors.push('Password is required')
+    passwordInput.parentElement.classList.add('incorrect')
+  }
+  return errors;
+}
+
+
+const allInputs = [nameInput, emailInput, passwordInput, addressInput, userIdInput].filter(input => input != null)
+
+allInputs.forEach(input => {
+  input.addEventListener('input', () => {
+    if(input.parentElement.classList.contains('incorrect')){
+      input.parentElement.classList.remove('incorrect');
+      errorMessage.innerText = ''
+    }
+  })
+})
