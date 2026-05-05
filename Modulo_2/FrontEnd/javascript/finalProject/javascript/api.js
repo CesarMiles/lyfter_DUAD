fc440751ac04457af1d51dd6b75c24a353bb7a7a
Cesar Miles <ca.miles@hotmail.com>
2026-05-04 19:37:21 -0600

    feat/Added additional functions to api.js, me, and authHeader to retrieve user profile information

--- Modulo_2/FrontEnd/javascript/finalProject/javascript/api.js
+++ Modulo_2/FrontEnd/javascript/finalProject/javascript/api.js
@@ -22,3 +22,48 @@ async function createUser(email, password) {
       return { success: false, error: error.message };
     } 
 }
+
+// Function to login as user checking if information is available
+async function login(email, password) {
+  try {
+    const userBody = {
+      'email' : email,
+      'password' : password
+    };
+
+    const response = await userInstance.post('/login', userBody);
+    return {success: true, data: response.data}
+  } catch (error) {
+    if (error.response) {
+        console.log('There was an error', error.response.status, error.response.data);
+      } else {
+        console.log('There was an error', error.message);
+      }
+      return { success: false, error: error.message };
+  }
+}
+
+function authHeader() {
+  const token = localStorage.getItem('authToken');
+  const result = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+  return result
+}
+
+// Function to get user information uses auth function to retrieve localstorage token 
+async function getMe() {
+  try {
+    const response = await userInstance.get('/me', authHeader());
+    return {success: true, data: response.data}
+  } catch (error) {
+    if (error.response) {
+        console.log('There was an error', error.response.status, error.response.data);
+      } else {
+        console.log('There was an error', error.message);
+      }
+      return { success: false, error: error.message };
+  }
+}
