54dc5d488417f40a416b7c72df67e14d79bef063
Cesar Miles <ca.miles@hotmail.com>
2026-06-06 14:19:55 -0600

    Feat/Adding auth.js to the project

--- Modulo_2/FrontEnd/javascript/finalProject/javascript/api.js
+++ Modulo_2/FrontEnd/javascript/finalProject/javascript/api.js
@@ -12,14 +12,19 @@ async function createUser(email, password) {
     };
 
     const response = await userInstance.post('/register', userBody);
-    return {success: true, data: response.data}
+    localStorage.setItem('authToken', response.data.token);
+    return {success: true, data: response.data};
   } catch (error) {
     if (error.response) {
-        console.log('There was an error', error.response.status, error.response.data);
-      } else {
+      console.log('There was an error', error.response.status, error.response.data);
+      if (error.response.status === 409) {
+        return {success: false, error: error.response.data.error}
+      };
+      return {success: false, error: "There was an error creating the user"}
+    } else {
         console.log('There was an error', error.message);
       }
-      return { success: false, error: error.message };
+      return { success: false, error: "There was an error connecting to the server" };
     } 
 }
 
@@ -32,17 +37,23 @@ async function login(email, password) {
     };
 
     const response = await userInstance.post('/login', userBody);
+    localStorage.setItem('authToken', response.data.token);
     return {success: true, data: response.data}
   } catch (error) {
     if (error.response) {
-        console.log('There was an error', error.response.status, error.response.data);
-      } else {
+      console.log('There was an error', error.response.status, error.response.data);
+      if (error.response.status === 403) {
+        return {success: false, error: "Invalid Credentials"}
+      };
+      return {success: false, error: "There was an error logging in"}
+    } else {
         console.log('There was an error', error.message);
       }
-      return { success: false, error: error.message };
-  }
+      return { success: false, error: "There was an error connecting to the server" };
+    } 
 }
 
+// Function used to build the bearer token used by the backed. Without this function the backed would deny each request.
 function authHeader() {
   const token = localStorage.getItem('authToken');
   const result = {
