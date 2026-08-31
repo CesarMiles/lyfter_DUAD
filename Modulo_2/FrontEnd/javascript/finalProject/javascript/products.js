5417dc61a1da794ce9fa67cd61a3b843a6d10c0d
Cesar Miles <ca.miles@hotmail.com>
2026-08-30 20:51:34 -0600

    feat/adding progress to cart function to save items to localstorage for the cart

--- Modulo_2/FrontEnd/javascript/finalProject/javascript/products.js
+++ Modulo_2/FrontEnd/javascript/finalProject/javascript/products.js
@@ -16,14 +16,32 @@ async function availableProducts() {
   const response = await getProducts();
   const available = response.data.filter((product) => product.stock > 0 );
   const cards = available.map(product => `<div class="product-card">
-        <img src="images/${productImages[product.product_name] || fallbackImage}" alt="">
+        <img src="images/${productImages[product.product_name] || fallbackImage}" alt="${product.product_name}">
         <div class="product-name-price">
           <p>${product.product_name}</p>
           <p>$${product.product_price}</p>
         </div>
-        <button type="button">Add to cart</button>
+        <button data-product-id="${product.product_id}" type="button">Add to cart</button>
       </div>`);
   productContainer.innerHTML = cards.join('');
 }
 
 availableProducts()
+
+productContainer.addEventListener('click', (event) => {
+  const rawId = event.target.dataset.productId;
+  if (!rawId) return;
+
+  const productId = Number(rawId);
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  
+  const item = cart.find((p) => p.product_id === productId);
+
+  if (item) {
+    item.quantity++;
+  } else {
+    cart.push({ product_id: productId, quantity: 1});
+  }
+
+  localStorage.setItem('cart', JSON.stringify(cart));
+})
